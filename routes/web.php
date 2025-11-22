4fad79c309751df9e04147dfce7f6bfac8edac63
Richie Reuben Hermanto <richiereubenh@gmail.com>
2025-11-22 19:27:45 +0700

    feat: add registration route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ParticipantController;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\RegistrationController;
+
 
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +12,5 @@ Route::get('/', function() { return redirect()->route('participants.index'); });
 Route::resource('participants', ParticipantController::class);
 Route::resource('courses', CourseController::class);
 
-
+Route::post('courses/{course}/register', [RegistrationController::class, 'store'])->name('courses.register');
+Route::delete('courses/{course}/register/{participant}', [RegistrationController::class, 'destroy'])->name('courses.register.destroy');
